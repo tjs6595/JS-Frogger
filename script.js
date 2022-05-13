8aeb59a69925dacfad4da40da7ba1565899c5ea1
tjs6595 <t.schultejans@schenckprocess.com>
2022-05-12 21:07:36 -0500

    Styled all obstacles.

--- script.js
+++ script.js
@@ -6,29 +6,50 @@ const startButton = document.createElement('button');
 startButton.textContent = "START";
 document.body.append(startButton);
 
-const boardWidth = 10;
-const boardHeight = 10;
-
+const gridSquareWidth = 10;
+const gridSquareHeight = 10;
 const squares = document.querySelectorAll('.grid div');
 
+//Set starting point for the frog.
 let currentIndex = 94;
 
-function moveFrog(){
+//Move the Frog based on arrow key press after checking boundaries.
+function moveFrog(e){
     switch (e.key){
         case 'ArrowLeft':
-            currentIndex = currentIndex -= 1;
+            if(currentIndex % gridSquareWidth !== 0){
+                squares[currentIndex].classList.remove('frog')
+                currentIndex = currentIndex -= 1;
+            }
             break;
         case 'ArrowRight':
-            currentIndex = currentIndex += 1;
+            if(currentIndex % gridSquareWidth !== 9){
+                squares[currentIndex].classList.remove('frog')
+                currentIndex = currentIndex += 1;
+            }
             break;
         case 'ArrowUp':
-            currentIndex = currentIndex -= 1;
+            if((currentIndex - gridSquareWidth) >= 0){
+                squares[currentIndex].classList.remove('frog')
+                currentIndex = currentIndex -= gridSquareWidth;
+            }
             break;
         case 'ArrowDown':
-            currentIndex = currentIndex - 1;
+            if((currentIndex + gridSquareWidth) <= (squares.length-1)){
+                squares[currentIndex].classList.remove('frog')
+                currentIndex = currentIndex += gridSquareWidth;
+            }
             break;
     }
     squares[currentIndex].classList.add('frog')
 
 }
-const keyPress = document.addEventListener('keypress', moveFrog)
+
+//Listen for an arrow key press, and then call the moveFrog function
+const keyPress = document.addEventListener('keydown', moveFrog)
+
+//Move Logs.
+
+//Move Cars.
+
+//Check Collisions.
